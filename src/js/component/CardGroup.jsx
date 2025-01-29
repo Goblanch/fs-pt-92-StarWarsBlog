82aca49a886f0c1d21654dfd1a192e000f3ccf4c
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2025-01-29 09:44:37 +0000

    Rename InfoCard and some visual changes

--- src/js/component/CardGroup.jsx
+++ src/js/component/CardGroup.jsx
@@ -1,12 +1,12 @@
 import React, { Children, useContext, useEffect } from "react";
 import { Context } from "../store/appContext.js";
-import InfoCard from "./InfoCard.jsx";
+import InfoCard from "./PersonCard.jsx";
 
 const CardGroup = ({ children, title }) => {
 
 
     return (
-        <div>
+        <div className="mb-3">
             <h2 className="text text-danger">{title}</h2>
             <div className="d-flex overflow-auto space-x-4 p-4">
                 {React.Children.map(children, (child, key) => (
